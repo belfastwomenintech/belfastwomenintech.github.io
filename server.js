ddd54ac9b458740847c61f8c84c1cb0400135ea3
Claire Wilgar <cwilgar@gmail.com>
2018-11-03 12:47:57 +0000

    Add basic styling

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.static(__dirname));
 
 app.listen('3000');
 console.log('working on 3000');
